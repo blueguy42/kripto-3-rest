5d9223c9585f57b9f2c14104ad25f215b7317a96
Ubaidillah Ariq <ubaidillahariq.rea@ruangguru.com>
2023-04-05 12:30:03 +0700

    refactor tapi tetep ngebug >:(

--- crypto/ecdsa.js
+++ crypto/ecdsa.js
@@ -1,177 +1,176 @@
+const bn = require('bn.js');
+const randomBytes = require('crypto').randomBytes
+
 const curve = {
-    p : 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2fn,
-    a : 0x0000000000000000000000000000000000000000000000000000000000000000n,
-    b : 0x0000000000000000000000000000000000000000000000000000000000000007n,
-    Gx : 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798n,
-    Gy : 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8n,
-    n : 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141n
+    p : uint256("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16),
+    a : uint256(0),
+    b : uint256(7),
+    Gx : uint256("79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 16),
+    Gy : uint256("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8", 16),
+    n : uint256("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", 16)
 };
 
-var getMultiplier = e => 10n ** BigInt(e)
-
-function multiply(a, b) {
-    let multiplier = getMultiplier(a["dec"]) * getMultiplier(b["dec"])
-    let ab;
-    if ((a["int"] > b["int"]) && (a["int"].toString().length > multiplier.toString().length)) {
-        ab = a["int"] / multiplier * b["int"]
-    } else if ((b["int"] > a["int"]) && (b["int"].toString().length > multiplier.toString().length)) {
-        ab = b["int"] / multiplier * a["int"]
-    } else if ((b["int"].toString().length + a["int"].toString().length) > multiplier.toString().length) {
-        ab = a["int"] * b["int"] / multiplier
-    } else {
-        let missing = multiplier.toString().length - (b["int"].toString().length + a["int"].toString().length) + 1
-        ab = a["int"] * b["int"] * getMultiplier(missing) / multiplier
-        // ab = Number(ab) / Number(getMultiplier(missing))
-        ab = Math.floor(ab / getMultiplier(missing))
-    }
-    return ab
-}
-
-function toInt(e) {
-    let eArray = e.split(".")
-    let pair = [];
-    pair["int"] = BigInt(eArray[0] + (eArray[1] ? eArray[1] : ""))
-    pair["dec"] = (eArray[1] ? eArray[1].length : 0)
-    return pair
-}
-
-// Check if a private key is valid
-function isValidPrivateKey(privateKey) {
-    const maxPrivateKey = curve.n - 1n;
-    return BigInt(privateKey) > 0n && BigInt(privateKey) <= maxPrivateKey;
+function uint256(x, base) {
+  return new bn(x, base)
 }
 
 // Generate a random private key
 async function getRandomPrivateKey() {
-    let privateKey;
-    do {
-        privateKey = multiply(toInt(Math.random().toString()), toInt(curve.n.toString()));
-    } while (!isValidPrivateKey(privateKey));
-    return privateKey.toString(16).padStart(64, '0');
+    return uint256(randomBytes(32)).umod(P).toString(16).padStart(64, '0');
 }
   
 // Compute the public key from the private key
 async function getPublicKey(privateKey) {
-    const d = BigInt('0x' + privateKey);
-    const G = { x: curve.Gx, y: curve.Gy };
-    const Q = pointMultiply(G, d, curve);
-
-    return Q.x.toString(16).padStart(64, '0') + Q.y.toString(16).padStart(64, '0');
+    const G = [curve.Gx, curve.Gy, uint256(1)]
+    let publicKey = pointMultiply(G, uint256(privateKey, 16), curve)
+    const inv = publicKey[2].invm(curve.p)
+    publicKey = [publicKey[0].mul(inv.mul(inv).umod(curve.p)).umod(curve.p), 
+                publicKey[1].mul(inv.mul(inv.mul(inv).umod(curve.p)).umod(curve.p)).umod(curve.p)]
+    // const pubX = uint256('5b75fd5f49e78191a45e1c9438644fe5d065ea98920c63e9eef86e151e99b809', 16)
+    // const pubY = uint256('4eef2a826f1e6d13a4dde4e54800e8d282a2089a873072002e0a3a21eae5763a', 16)
+    // const pk = pubX.toString(16).padStart(64, '0') + pubY.toString(16).padStart(64, '0');
+    // const sig = sign("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8",
+    //       "79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798")
+    // const valid = verify("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8", sig, pk )
+    // console.log(valid)
+    return publicKey[0].toString(16).padStart(64, '0') + publicKey[1].toString(16).padStart(64, '0');
 }
 
-// Compute the modular inverse of a number
-function modInverse(a, m) {
-    let [x, y] = [0n, 1n];
-    let [lastX, lastY] = [1n, 0n];
-    let [q, r] = [];
-  
-    while (a !== 0n) {
-      q = m / a;
-      r = m % a;
-      [m, a] = [a, r];
-      [x, lastX] = [lastX - q * x, x];
-      [y, lastY] = [lastY - q * y, y];
-    }
-  
-    return lastX < 0n ? lastX + m : lastX;
+function mulmod(a, b, P) {
+  return a.mul(b).umod(P)
+}
+
+function addmod(a, b, P) {
+  return a.add(b).umod(P)
+}
+
+function invmod(a, P) {
+  return a.invm(P)
+}
+
+function negmod(a, P) {
+  return P.sub(a)
 }
 
 // Add two points on an elliptic curve
 function pointAdd(point1, point2, curve) {
-    if (point1 === null) {
-      return point2;
-    }
-  
-    if (point2 === null) {
-      return point1;
-    }
-  
-    if (point1.x === point2.x && point1.y === point2.y) {
-      const slope = (3n * point1.x ** 2n + curve.a) * modInverse(2n * point1.y, curve.p);
-      const x = (((slope ** 2n - 2n * point1.x) % curve.p) + curve.p) % curve.p;
-      const y = (((slope * (point1.x - x) - point1.y) % curve.p) + curve.p) % curve.p;
-      return { x, y };
-    }
-  
-    if (point1.x === point2.x && point1.y === curve.p - point2.y) {
-      return null;
-    }
-  
-    const slope = (point2.y - point1.y) * modInverse(point2.x - point1.x, curve.p);
-    const x = (((slope ** 2n - point1.x - point2.x) % curve.p) + curve.p) % curve.p;
-    const y = (((slope * (point1.x - x) - point1.y) % curve.p ) + curve.p) % curve.p;
-  
-    return { x, y };
+  if (point2[0] == 0 && point2[1] == 0 && point2[2] == 0) {
+      return point1
+  }
+
+  let z2 = mulmod(point2[2], point2[2], curve.p)
+  const u1 = mulmod(point1[0], z2, curve.p)
+  const s1 = mulmod(point1[1], mulmod(z2, point2[2], curve.p), curve.p)
+  z2 = mulmod(point1[2], point1[2], curve.p)
+  let u2 = mulmod(point2[0], z2, curve.p)
+  let s2 = mulmod(point2[1], mulmod(z2, point1[2], curve.p), curve.p)
+
+  if (u1.eq(u2)) {
+      if (!s1.eq(s2)) {
+          return [_1, _1, _0]
+      }
+      else {
+        const z2 = mulmod(point1[2], point1[2], curve.p)
+        const m = addmod(mulmod(curve.a, mulmod(z2, z2, curve.p), curve.p), mulmod(uint256(3), mulmod(point1[0], point1[0], curve.p), curve.p), curve.p)
+        const y2 = mulmod(point1[1], point1[1], curve.p)
+        const s = mulmod(uint256(4), mulmod(point1[0], y2, curve.p), curve.p)
+        const x = addmod(mulmod(m, m, curve.p), negmod(mulmod(s, uint256(2), curve.p), curve.p), curve.p)
+
+        return [
+            x,
+            addmod(mulmod(m, addmod(s, negmod(x, curve.p), curve.p), curve.p), negmod(mulmod(uint256(8), mulmod(y2, y2, curve.p), curve.p), curve.p), curve.p),
+            mulmod(uint256(2), mulmod(point1[1], point1[2], curve.p), curve.p)
+        ]
+      }
+  }
+
+  u2 = addmod(u2, negmod(u1, curve.p), curve.p)
+  z2 = mulmod(u2, u2, curve.p)
+  const t2 = mulmod(u1, z2, curve.p)
+  z2 = mulmod(u2, z2, curve.p)
+  s2 = addmod(s2, negmod(s1, curve.p), curve.p)
+  const x = addmod(addmod(mulmod(s2, s2, curve.p), negmod(z2, curve.p), curve.p), negmod(mulmod(uint256(2), t2, curve.p), curve.p), curve.p)
+
+  return [
+      x,
+      addmod(mulmod(s2, addmod(t2, negmod(x, curve.p), curve.p), curve.p), negmod(mulmod(s1, z2, curve.p), curve.p), curve.p),
+      mulmod(u2, mulmod(point1[2], point2[2], curve.p), curve.p)
+  ]
 }
 
 // Multiply a point by a scalar on an elliptic curve
 function pointMultiply(point, scalar, curve) {
-    let result = null;
+    let result = [uint256(0), uint256(0), uint256(0)];
     let addend = point;
   
-    while (scalar > 0n) {
-      if (scalar & 1n) {
-        result = pointAdd(result, addend, curve);
-      }
-  
-      addend = pointAdd(addend, addend, curve);
-      scalar >>= 1n;
+    if (scalar == 0 || ((point[0] == 0) && (point[1] == 0)) ) {
+        return result;
     }
-  
+
+    const d = scalar.clone()
+    while (d != 0) {
+        if (d.testn(0)) {
+            result = pointAdd(addend, result, curve);
+        }
+        addend = pointAdd(addend, addend, curve);
+        d.iushrn(1);
+    }
+
     return result;
 }
 
 // Compute the signature
 async function sign(hash, privateKey) {
-    let r, s;
-    do {
-        const k = multiply(toInt(Math.random().toString()), toInt(curve.n.toString()));
-        const G = { x: curve.Gx, y: curve.Gy };
-        const Q = pointMultiply(G, k, curve);
-        const x = ((Q.x % curve.n) + curve.n) % curve.n;
-        r = x === 0n ? sign(hash, privateKey) : x;
+    while (true) {
+        const k = uint256(randomBytes(32), 16).umod(curve.p);
+        const G = [curve.Gx, curve.Gy, uint256(1)];
+        const r = pointMultiply(G, k, curve);
+        if (r[0] == 0) continue;
 
-        const e = BigInt('0x' + hash);
-        const d = BigInt('0x' + privateKey);
-        s = ((modInverse(k, curve.n) * (e + d * r) % curve.n) + curve.n) % curve.n;
-    } while (r === 0n || s === 0n);
-
-    const signature = {
-        r: r.toString(16).padStart(64, '0'),
-        s: s.toString(16).padStart(64, '0')
-    };
-    return signature;
+        const e = uint256(hash, 16);
+        const d = uint256(privateKey, 16);
+        s = mulmod(k.invm(curve.n), addmod(e, mulmod(r[0], d, curve.n), curve.n), curve.n);
+        if (s == 0) continue;
+        if (s.testn(255)) continue;
+        return {
+          r: r[0].toString(16).padStart(64, '0'),
+          s: s.toString(16).padStart(64, '0')
+        };
+    }
 }
 
 // Verify the signature
 async function verify(hash, signature, publicKey) {
-    const r = BigInt('0x' + signature.r);
-    const s = BigInt('0x' + signature.s);
-    const e = BigInt('0x' + hash);
-    const Q = {
-        x: BigInt('0x' + publicKey.slice(0, 64)),
-        y: BigInt('0x' + publicKey.slice(64))
-    };
+    const r = uint256(signature.r, 16);
+    const s = uint256(signature.s, 16);
+    const e = uint256(hash, 16);
+    const Q = [uint256(publicKey.slice(0, 64), 16), uint256(publicKey.slice(64), 16), uint256(1)]
 
-    if (r <= 0n || r >= curve.n || s <= 0n || s >= curve.n) {
-        return false;
-    }
+    // console.log(r)
+    // console.log(s)
 
-    const w = modInverse(s, curve.n);
-    const u1 = (((e * w) % curve.n) + curve.n) % curve.n;
-    const u2 = (((r * w) % curve.n) + curve.n) % curve.n;
-    const G = { x: curve.Gx, y: curve.Gy };
-    const P = pointAdd(pointMultiply(G, u1, curve), pointMultiply(Q, u2, curve), curve);
+    // if (r <= 0 || r >= curve.n || s <= 0 || s >= curve.n) {
+    //     return false;
+    // }
+
+    const w = s.invm(curve.n);
+    const u1 = mulmod(e, w, curve.n)
+    const u2 = mulmod(r, w, curve.n)
+    const G = [curve.Gx, curve.Gy, uint256(1)]
+
+    let P = pointAdd(pointMultiply(G, u1, curve), pointMultiply(Q, u2, curve), curve);
+    const inv = P[2].invm(curve.p)
+    P = [mulmod(P[0], mulmod(inv, inv, curve.p), curve.p),
+         mulmod(P[1], mulmod(inv, mulmod(inv, inv, curve.p), curve.p), curve.p)]
+
+    // console.log(r)
+    // console.log(P[0])
 
     if (P === null) {
         return false;
     }
 
-    console.log(getPublicKey('d9576534d36bed4b1e8923b55dd0fb5a1a0a2cce9982c77b0778b6f0ac1b33a4'))
-    console.log(r)
-    console.log((((P.x % curve.n) + curve.n) % curve.n))
-
-    return r === (((P.x % curve.n) + curve.n) % curve.n);
+    return r.eq(P[0]);
 }
 
 module.exports = {
--- routes/key.js
+++ routes/key.js
@@ -83,5 +83,36 @@ router.get('/generateKey', async (req, res) => {
   }
 });
 
+router.post('/getPublicKey', async (req, res) => {
+  try {
+    const { privateKey } = req.body;
+    const publicKey = await crypto.ecdsa.getPublicKey(privateKey);
+    res.status(200).json({ publicKey: publicKey });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: err.message });
+  }
+});
+
+router.get('/test', async (req, res) => {
+  try {
+    const bn = require('bn.js');
+    function uint256(x, base) {
+      return new bn(x, base)
+    }
+    const pubX = uint256('5b75fd5f49e78191a45e1c9438644fe5d065ea98920c63e9eef86e151e99b809', 16)
+    const pubY = uint256('4eef2a826f1e6d13a4dde4e54800e8d282a2089a873072002e0a3a21eae5763a', 16)
+    const pk = pubX.toString(16).padStart(64, '0') + pubY.toString(16).padStart(64, '0');
+    const sig = await crypto.ecdsa.sign("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8",
+          "79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798")
+    const valid = await crypto.ecdsa.verify("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8", sig, pk )
+    console.log(valid)
+    res.status(200).json({ valid: valid });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: err.message });
+  }
+});
+
 
 module.exports = router;
