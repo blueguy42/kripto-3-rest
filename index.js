3026b29aae97993adc1ec3045efad92f614c853d
Ahmad Alfani Handoyo <70305222+blueguy42@users.noreply.github.com>
2023-04-10 21:48:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.get('/api/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
